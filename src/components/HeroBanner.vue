bd74330fb667ede3df78ab95c6d26a5a82f73e4f
GowellKent <Gowellkent1@gmail.com>
2022-09-30 17:33:03 +0700

    adding card component

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,18 +1,50 @@
 <template>
-    <div class="row mt-4">
-        <div class="col">
-            <div class="d-flex h-100">
-                <div class="justify-content-center align-self-center">
-                    <h2><strong>Readily Available</strong> <br />Shopping Made Easy!</h2>
-                    <p>Do your groceries from the palm of your hands</p>
-                    <button class="btn btn-lg btn-primary"><b-icon-arrow-right></b-icon-arrow-right> Order</button>
+    <div class="hero">
+        <!-- Desktop -->
+        <div class="d-none d-md-block">
+            <div class="row mt-4">
+                <div class="col-md">
+                    <div class="d-flex h-100">
+                        <div class="justify-content-center align-self-center">
+                            <h2><strong>Readily Available</strong> <br />Shopping Made Easy!</h2>
+                            <p>Do your groceries from the palm of your hands</p>
+                            <button class="btn btn-lg btn-primary">
+                                <b-icon-arrow-right></b-icon-arrow-right> Order
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <img src="@/assets/images/undraw_shopping_app_flsj.png" alt="@/assets/images/default.png"
+                        width="100%">
                 </div>
             </div>
         </div>
-        <div class="col">
-            <img src="@/assets/images/undraw_shopping_app_flsj.png" alt="@/assets/images/default.png" width="100%">
+
+
+        <!-- Mobile -->
+        <div class="d-sm-block d-md-none">
+            <div class="row mt-4">
+                <div class="col-md mb-3">
+                    <img src="@/assets/images/undraw_shopping_app_flsj.png" alt="@/assets/images/default.png"
+                        width="100%">
+                </div>
+                <div class="col-md">
+                    <div class="d-flex h-100">
+                        <div class="justify-content-center align-self-center">
+                            <h2><strong>Readily Available</strong> <br />Shopping Made Easy!</h2>
+                            <p>Groceries from the palm of your hands</p>
+                            <button class="btn btn-lg btn-primary">
+                                <b-icon-arrow-right></b-icon-arrow-right> Order
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
+
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="home">
-      <Navbb />
-      <div class="container">
-        <HerBan />
+    <Navbb />
+    <div class="container">
+      <HerBan />
+      <div class="row mt-4">
+        <div class="col">
+          <h2><strong>Best</strong> Sellers</h2>
+        </div>
+        <div class="col">
+          <router-link to="/products" class="btn btn-primary float-right">
+            <b-icon-bag></b-icon-bag> Shop Now
+          </router-link>
+        </div>
       </div>
+
+      <div class="row mb-3">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+        <div class="col-md-4 mt-4">
+          <h2>item2</h2>
+        </div>
+        <div class="col-md-4 mt-4">
+          <h2>itme3</h2>
+        </div>
+      </div>
+
+
+    </div>
   </div>
 </template>
 
@@ -11,11 +35,36 @@
 // @ is an alias to /src
 import Navbb from '@/components/NavigationBar'
 import HerBan from '@/components/HeroBanner'
+import CardProduct from '@/components/CardProduct'
+import axios from "axios"
 
 export default {
   name: 'HomeView',
   components: {
-    Navbb, HerBan
+    Navbb,
+    HerBan,
+    CardProduct
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/best-products')
+      .then(function (response) {
+        // handle success
+        this.setProduct(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Gagal ", error);
+      })
   }
 }
 </script>
